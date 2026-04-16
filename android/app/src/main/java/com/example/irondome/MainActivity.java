2713ba9d5a7cbe615991e6b20fa681d462cf6a37
avdas829-afk <avdas829@gmail.com>
2026-04-16 17:07:53 +0545

    feat(android): Set up basic Android WebView application
    This commit configures the Android project to launch a WebView that loads a local index.html file. It includes necessary build scripts, manifest configurations, and initializes the WebView with JavaScript support.

---
+++ android/app/src/main/java/com/example/irondome/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.irondome;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        
+        // Ensure local files can be loaded
+        webSettings.setAllowFileAccessFromFileURLs(true);
+        webSettings.setAllowUniversalAccessFromFileURLs(true);
+
+        webView.setWebViewClient(new WebViewClient());
+        
+        // Load the local index.html from assets
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
